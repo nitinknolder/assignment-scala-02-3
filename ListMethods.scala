4d607b1d0d135c575d3ed18e60369aeb3841d03c
nitinknolder <35076408+nitinknolder@users.noreply.github.com>
2018-01-18 17:38:45 +0530

    updated file

--- ListMethods.scala
+++ ListMethods.scala
@@ -1,4 +1,4 @@
-﻿package edu.knoldus
+package edu.knoldus
 
 import org.apache.log4j.Logger
 
@@ -13,58 +13,69 @@ class ListMethods {
     firstList ++ secondList
   }
 
+  def findSubSequences[A] (newList: List[A], subList: List[A]): Boolean = {
 
-  def findSubSequences[A] (newList: List[A], subList: List[A], updatedList: List[A]): Boolean = newList match {
+    def subSequence[A] (newList: List[A], subList: List[A]): Boolean = subList match {
+      case head :: tail if head == newList.head => subSequence (newList.tail, tail)
+      case head :: tail if head != newList.head => false
+      case _ => true
+    }
 
-    case head :: tail if head == subList.head => findSubSequences (tail, subList.tail, updatedList)
-    case head :: tail if head != subList.head && tail != subList.head => findSubSequences (tail, updatedList, updatedList)
-    case head :: tail if head != subList.head && tail == subList.head => findSubSequences (newList, tail, updatedList)
-    //  case head :: tail if(head != subList.head && subList.head == updatedList.head) => findSubSequences(updatedList,newList,updatedList)
-    case _ => false
+    newList match {
+      case head :: tail if head == subList.head => {
+        subSequence (tail, subList.tail) match {
+          case true => true
+          case false => findSubSequences (tail, subList)
+        }
+      }
+      case head :: tail if head != subList.head => findSubSequences (tail, subList)
+      case head :: Nil => true
+      case _ => false
+    }
   }
 
-   def splitList[A] (originalList: List[A], f: A => Boolean): (List[A], List[A]) = {
+/*  def splitList[A] (originalList: List[A], f: A => Boolean): (List[A], List[A]) = {
 
-     def listPartition[A](originalList: List[A],firstPartition: List[A],secondPartition: List[A]) = {
+    def listPartition[A] (originalList: List[A], firstPartition: List[A], secondPartition: List[A]): List[A] = {
 
-       originalList match {
+      originalList match {
 
-         case firstnum :: restNum if f(firstnum) => listPartition(restNum,firstnum :: firstPartition,secondPartition)
-         case firstnum :: restNum if f(firstnum) => listPartition(restNum,firstPartition,firstnum::secondPartition)
-         case Nil => (firstPartition,secondPartition)
-       }
+        case firstnum :: restNum if f (firstnum) => listPartition (restNum, firstnum :: firstPartition, secondPartition)
+        case firstnum :: restNum if f (firstnum) => listPartition (restNum, firstPartition, firstnum :: secondPartition)
+        case Nil => firstPartition
+      }
+    }
 
-     }
-
-   }
+  }*/
 }
+
 
 object listMethod1 {
 
   def main (args: Array[String]) {
 
     val newObj = new ListMethods
-    val log = Logger.getLogger(getClass)
+    val log = Logger.getLogger (getClass)
     val val1 = 20
     val val2 = 30
     val val3 = 40
     val val4 = 50
-    val newList = List (val1, val2, val3, val4) //for Integers
+
+    val newList = List (val1, val2, val3, val4,7,8) //for Integers
     val str1 = "a"
     val str2 = "b"
-    val newList1 = List (str1,str2) //for Strings
-    val subList = List (val1,val2)
+    val newList1 = List (str1, str2) //for Strings
+    val val6 = 60
+    val subList = List (val3,val4)
     val updatedList = List ()
-    val originalList = List (val1,val2,val3,val4)
-    log.info ("/n" + newObj.findLengthOfList (newList))
-    log.info("/n" + newObj.findLengthOfList (newList1))
-    val firstList = List (val1,val2)
-    val secondList = List (val1,val2)
-    log.info("/n" + newObj.concatenationOfList (firstList, secondList))
-    log.info("/n" + newObj.findSubSequences (newList, subList, updatedList))
-    log.info("\n" + newObj.splitList(originalList,(function: Int) => function > 5))
-
-
+    val originalList = List (val1, val2, val3, val4)
+    log.info ("\n" + newObj.findLengthOfList (newList))
+    log.info ("\n" + newObj.findLengthOfList (newList1))
+    val firstList = List (val1, val2,43,45)
+    val secondList = List (val1, val2)
+    log.info ("\n" + newObj.concatenationOfList (firstList, secondList))
+    log.info ("\n" + newObj.findSubSequences (newList, subList))
+    //  log.info("\n" + newObj.splitList(originalList,(function: Int) => function > 30))
   }
 }
 
